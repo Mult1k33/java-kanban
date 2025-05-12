700888377efcca3a3fcf318d47d91aaedf8ebaf9
Дмитрий Кан <dmitry.kan@yandex.ru>
2025-05-12 19:52:08 +0300

    fix: Исправить CodeStyle

--- src/service/handlers/HistoryHandler.java
+++ src/service/handlers/HistoryHandler.java
@@ -1,9 +1,7 @@
 package service.handlers;
 
-import com.google.gson.JsonSyntaxException;
 import com.sun.net.httpserver.HttpExchange;
 import model.Task;
-import service.exeptions.NotFoundException;
 import service.managers.TaskManager;
 
 import java.io.IOException;
